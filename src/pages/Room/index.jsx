e1fe05a0da03ac685d743a4248d2b90aee505d3d
VaibhavSharma1998 <sharmavaibhav203@gmail.com>
2024-01-04 16:17:33 +0530

    "kind of done but not getting kick"

--- src/pages/Room/index.jsx
+++ src/pages/Room/index.jsx
@@ -1,7 +1,43 @@
-import React from 'react'
+import React from "react";
+
+import { useParams } from "react-router-dom";
+
+import { ZegoUIKitPrebuilt } from "@zegocloud/zego-uikit-prebuilt";
 
 export const RoomPage = () => {
+  const { roomId } = useParams();
+
+  const myMeeting = async (element) => {
+    const appID = 1115829804;
+    const serverSecret = "94d83c60470acafe45bcc3c444c9fc4b";
+    const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
+      appID,
+      serverSecret,
+      roomId,
+      Date.now().toString(),
+      "Vaibhav Sharma"
+    );
+
+    // Create instance object from Kit Token.
+    const zc = ZegoUIKitPrebuilt.create(kitToken);
+    // start the call
+    zc.joinRoom({
+      container: element,
+      sharedLinks: [
+        {
+          name: "Copy Link",
+          url: `http://localhost:3000/room/${roomId}`,
+        },
+      ],
+      scenario: {
+        mode: ZegoUIKitPrebuilt.OneONoneCall,
+      },
+      showScreenSharingButton: true,
+    });
+  };
   return (
-    <h1>RoomPage</h1>
-  )
-}
+    <div>
+      <div ref={myMeeting} />
+    </div>
+  );
+};
