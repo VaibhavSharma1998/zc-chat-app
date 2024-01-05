a7179aac978c02c9c128edfa1fb6ff059ce714cd
VaibhavSharma1998 <sharmavaibhav203@gmail.com>
2024-01-05 19:55:03 +0530

    "getting some error in 2nd person videocall"

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useCallback } from "react";
 import { useNavigate } from "react-router-dom";
+import "./index.css";
+import VideoCallMainImg from "../../images/videoCallMainImg.jpeg";
 
 export const HomePage = () => {
   const [value, setValue] = useState();
@@ -9,16 +11,39 @@ export const HomePage = () => {
     navigate(`/room/${value}`);
   }, [navigate, value]);
   return (
-    <div>
-      <input
-        type="text"
-        name="text"
-        id="text"
-        placeholder="Enter Room Id"
-        value={value}
-        onChange={(e) => setValue(e.target.value)}
-      />
-      <button onClick={handleClick}>Join</button>
-    </div>
+    <main>
+    <section>
+     
+      <div className="container">
+      
+        <div className="row main-div">
+        <h3 className="text-center">Enter my sweetheart your name to start video call ❤️</h3>
+          <div className="col-lg-6">
+            <img src={VideoCallMainImg} alt="video-call" className="img-fluid main-img" />
+          </div>
+          <div className="col-lg-6">
+            <input
+              type="text"
+              name="text"
+              id="text"
+              required
+              placeholder="Enter your name to start video call"
+              value={value}
+              onChange={(e) => setValue(e.target.value)}
+              className="input form-control" 
+              
+            />
+            <div className="text-center">
+            <button onClick={handleClick} className="join-btn btn btn-primary mt-5 "> 
+              Join
+            </button>
+            </div>
+           
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
+  
   );
 };
